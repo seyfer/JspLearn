5e4f0339f206d1430115e391fc9492098256611b
seyfer <seyferseed@mail.ru>
2016-04-09 22:46:07 +0700

    fix

--- src/picturesquirrel/controller/MainController.java
+++ src/picturesquirrel/controller/MainController.java
@@ -2,7 +2,10 @@ package picturesquirrel.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,32 +17,59 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/MainController")
 public class MainController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private Map<String, String> actionMap = new HashMap<>();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		getServletContext().getRequestDispatcher("/picturesquirrel/home.jsp").forward(request, response);
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public MainController() {
+		// Build a Map of action parameters to pages
+
+		actionMap.put("image", "/image.jsp");
+		actionMap.put("rate", "/image.jsp");
+		actionMap.put("home", "/home.jsp");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see Servlet#init(ServletConfig)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
+	private void doForward(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Get the action parameter
+		String action = request.getParameter("action");
+
+		// If the action parameter is null or the map doesn't contain
+		// a page for this action, set the action to the home page
+		if (action == null || !actionMap.containsKey(action)) {
+			action = "home";
+		}
+
+		// Forward to the requested page.
+		request.getRequestDispatcher("/picturesquirrel/" + actionMap.get(action)).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doForward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doForward(request, response);
+	}
 }
